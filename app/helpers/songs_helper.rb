d6da38886ecf02317a3c4658570ded4e7172c488
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-12 15:08:06 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+    # def artist_select
+    #     if song.author.nil?
+    #         select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, name)
+    #     else
+    #         hidden_field_tag "post[author_id]", post.author_id
+    #     end
+    # end
+
 end
